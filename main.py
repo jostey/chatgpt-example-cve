bf9daa1d6b467c4ede1bb32f15fd7d87c3378c7f
Jostey <jostey96@gmail.com>
2023-03-14 18:48:07 +0100

    v.0.2: fixed json format in input

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ openai.api_key = "API_KEY"
 def generate_prompt(input: str) -> str:
     return """Obtener productos y versiones en ESPAÑOL en formato JSON. Ejemplos.
     Q: IBM Business Automation Workflow 18.0.0, 18.0.1, 18.0.2, 19.0.1, 19.0.2, 19.0.3, 20.0.1, 20.0.2, 20.0.3, 21.0.1, 21.0.2, 21.0.3 y 22.0.1 es vulnerable a la falsificación de solicitudes entre sitios que podría permitir a un atacante ejecutar acciones maliciosas y no autorizadas transmitidas desde un usuario en el que confía el sitio web.
-    A: {{\"product\": \"IBM Business Automation Workflow\", \"versions\": \"18.0.0, 18.0.1, 18.0.2, 19.0.1, 19.0.2, 19.0.3, 20.0.1, 20.0.2, 20.0.3, 21.0.1, 21.0.2, 21.0.3 y 22.0.1\"}}
+    A: {{"product": "IBM Business Automation Workflow", "versions": "18.0.0, 18.0.1, 18.0.2, 19.0.1, 19.0.2, 19.0.3, 20.0.1, 20.0.2, 20.0.3, 21.0.1, 21.0.2, 21.0.3 y 22.0.1"}}
     Q: A double-free was found in the way 389-ds-base handles virtual attributes context in persistent searches.
-    A: {{\"product\": \"389-ds-base\", \"versions\": \"No se indica.\"}}
+    A: {{"product": "389-ds-base", "versions": "No se indica."}}
     Q: Cross-site Scripting (XSS) - Stored in GitHub repository pimcore/pimcore prior to 10.5.19.
-    A: {{\"product\": \"pimcore\", \"versions\": \"antes de 10.5.19.\"}}
+    A: {{"product": "pimcore", "versions": "antes de 10.5.19."}}
     Q: A issue has been discovered in GitLab CE/EE affecting all versions from 15.3 prior to 15.7.8, version 15.8 prior to 15.8.4, and version 15.9 prior to 15.9.2 A cross-site scripting vulnerability was found in the title field of work it...
-    A: {{\"product\": "GitLab CE/EE", \"versions\": \"15.3-15.7.8, 15.8-15.8.4 y 15.9-15.9.2\"}}
+    A: {{"product": "GitLab CE/EE", "versions": "15.3-15.7.8, 15.8-15.8.4 y 15.9-15.9.2"}}
     Q: {}
     A: """.format(input)
 
