78faf768bbe7c496d8b2825b5a16d4371baee5d4
Jostey <jostey96@gmail.com>
2023-03-14 15:38:57 +0100

    Correctly formatted code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ INPUT = "github-slug-action is a GitHub Action to expose slug value of GitHub en
 # https://platform.openai.com/account/api-keys
 openai.api_key = "API_KEY"
 
+
 def generate_prompt(input):
     return """Obtener productos y versiones en ESPAÑOL. Ejemplos.
     Q: IBM Business Automation Workflow 18.0.0, 18.0.1, 18.0.2, 19.0.1, 19.0.2, 19.0.3, 20.0.1, 20.0.2, 20.0.3, 21.0.1, 21.0.2, 21.0.3 y 22.0.1 es vulnerable a la falsificación de solicitudes entre sitios que podría permitir a un atacante ejecutar acciones maliciosas y no autorizadas transmitidas desde un usuario en el que confía el sitio web.
@@ -17,8 +18,8 @@ def generate_prompt(input):
     Q: A issue has been discovered in GitLab CE/EE affecting all versions from 15.3 prior to 15.7.8, version 15.8 prior to 15.8.4, and version 15.9 prior to 15.9.2 A cross-site scripting vulnerability was found in the title field of work it...
     A: GitLab CE/EE 15.3-15.7.8, 15.8-15.8.4 y 15.9-15.9.2
     Q: {}
-    A: """.format(
-        input)
+    A: """.format(input)
+
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
